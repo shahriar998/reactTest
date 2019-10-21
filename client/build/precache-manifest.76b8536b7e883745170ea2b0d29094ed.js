eff871f819fdc2de185ae26cdbdd18b9b42190d7
Shahriar Elias <shahriar998@gmail.com>
2019-10-21 19:38:58 -0400

    trying it again

---
+++ client/build/precache-manifest.76b8536b7e883745170ea2b0d29094ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a201b23c05ac59618f0949ed407c35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65c9a96f304e788fe43a",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8a9757a4f4bb0fde0e2e",
+    "url": "/static/js/2.685cf6f1.chunk.js"
+  },
+  {
+    "revision": "65c9a96f304e788fe43a",
+    "url": "/static/js/main.f39a0ca9.chunk.js"
+  },
+  {
+    "revision": "96d9506c0ec10f86d92f",
+    "url": "/static/js/runtime-main.a245edba.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
